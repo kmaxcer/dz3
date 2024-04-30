5d8b1bb70e9a85574e29806cc9be40ac63406069
kmaxcer <149956842+kmaxcer@users.noreply.github.com>
2024-04-30 09:52:54 +0300

    com3

--- adminka/app.js
+++ adminka/app.js
@@ -1,17 +1,23 @@
-     // app.js 
-     const express = require('express');
-     const path = require('path'); 
-     const bodyParser = require('body-parser');
-     const mainRoute = require('./routes/main');
-     const gamesRouter = require('./routes/games'); 
-     
-     const PORT = 3000;
-     const app = express();
-     
-     // Запускаем
-     app.use(mainRoute, gamesRouter, bodyParser.json()); 
-     app.use(express.static(path.join(__dirname, 'public')));
-     app.listen(PORT, () => {
-       console.log(`Server is running at PORT http://localhost:${PORT}`);
-     })
-     
+ // app.js 
+ const express = require('express');
+ const path = require('path');
+ // Импортируем библиотеку
+ const bodyParser = require('body-parser');
+ const mainRoute = require('./routes/main');
+ const gamesRouter = require('./routes/games'); 
+ 
+ const PORT = 3000;
+ const app = express();
+ 
+ // Настраиваем миддлвар-функцию bodyParser
+ app.use(
+     bodyParser.json(),
+     express.static(path.join(__dirname, 'public')),
+     mainRoute,
+     gamesRouter
+ ); 
+ 
+ app.listen(PORT, () => {
+     console.log(`Server is running at PORT http://localhost:${PORT}`);
+ })
+ 
